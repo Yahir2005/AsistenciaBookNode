0bed053cc193e2a687b9b1c52d74199c733619c1
Yahir2005 <luisyajairo@outlook.com>
2026-02-28 13:37:46 -0600

    Asistenes modelo

---
+++ agendaapi/src/models/Asistentes.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const dbConn = require("../../config/dbConfig");
+
+const Asistentes = function (asistente) {
+    this.id_asistente = asistente.id_asistente;
+    this.Nombre = asistente.Nombre;
+    this.Apellido = asistente.Apellido;
+    this.Email = asistente.Email;
+    this.Telefono = asistente.Telefono;
+    this.DocumentoIdentidad = asistente.DocumentoIdentidad;
+    this.TipoDocumento = asistente.TipoDocumento;
+    this.FechaNacimiento = asistente.FechaNacimiento;
+    this.Genero = asistente.Genero;
+    this.Profesion = asistente.Profesion;
+    this.Institucion = asistente.Institucion;
+    this.Ciudad = asistente.Ciudad;
+    this.Pais = asistente.Pais;
+    this.CodigoQR = asistente.CodigoQR;
+    this.Activo = asistente.Activo;
+    this.FechaRegistro = asistente.FechaRegistro;
+    this.Notas = asistente.Notas;
+}
+Asistentes.listarAsistentes = function (result) {
+    dbConn.query("SELECT * FROM Asistentes", function (err, res) {
+        if (err) {
+            console.log("error al consultar", err);
+        } else {
+            console.log("Asistentes obtenidos", res);
+        }
+    }
+    )
+}
