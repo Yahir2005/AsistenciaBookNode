a624172087315c46d39c34c1172e6fc35d6a0850
Yahir2005 <luisyajairo@outlook.com>
2026-03-04 21:57:46 -0600

    rutas

--- agendaapi/src/controller/AsistentesController.js
+++ agendaapi/src/controller/AsistentesController.js
@@ -2,20 +2,18 @@ const Asistentes = require("../models/Asistentes");
 const RespuestaBasica = require("../models/Response");
 
 exports.listarAsistentes = function (req, res) {
-    Asistentes.listarAsistentes(function (err, res) {
+    Asistentes.listarAsistentes(function (err, result) {
         var respuestaRetorno = new RespuestaBasica();
         if (err) {
-            console.log("error al consultar", res);
             respuestaRetorno.status = false;
-            respuestaRetorno.code = 200;
             respuestaRetorno.message = "Error al consultar los asistentes";
         } else {
-            console.log("Asistentes obtenidos", res);
             respuestaRetorno.status = true;
             respuestaRetorno.code = 200;
             respuestaRetorno.message = "Asistentes obtenidos correctamente";
+            respuestaRetorno.data = result;
             res.status(200);
             res.send(respuestaRetorno);
         }
-    })
-}
+    });
+};
