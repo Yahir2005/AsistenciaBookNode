2527a8a01fb5efb301d9262ed45572e55c9b065b
Yahir2005 <luisyajairo@outlook.com>
2026-03-04 22:35:15 -0600

    AsistentesRouter

--- agendaapi/routes/Asistentes.js
+++ agendaapi/routes/Asistentes.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var AsistentesController = require("../src/controller/AsistentesController");
 
 router.get("/", AsistentesController.listarAsistentes);
+router.get("/",AsistentesController.RegistrarAsistente);
 
 module.exports = router;
