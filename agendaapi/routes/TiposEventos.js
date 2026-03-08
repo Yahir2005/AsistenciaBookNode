f3da65ac3078b07f1901c8975d6dd06e553fabd7
Yahir2005 <luisyajairo@outlook.com>
2026-03-08 01:11:51 -0600

    TiposEventosRouter

---
+++ agendaapi/routes/TiposEventos.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var router = express.Router();
+
+var TiposEventosController = require("../src/controller/TiposEventosController");
+
+router.get("/",TiposEventosController.listarTiposEventos);
+
+module.exports = router;
